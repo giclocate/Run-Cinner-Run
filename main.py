587c9322d195282c7a78113b252392360831e711
EricHayato <eric.makiyama@gmail.com>
2024-03-13 19:35:31 -0300

    Rocha, colisão e SFX de colisão

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ pygame.mixer.init()
 diretorio_principal = os.path.dirname(__file__)
 diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
 diretorio_sons = os.path.join(diretorio_principal, 'Sounds')
+diretorio_background = os.path.join(diretorio_principal, diretorio_sons, 'Background')
 LARGURA = 640
 ALTURA = 480
 
@@ -21,7 +22,12 @@ pygame.display.set_caption('Projeto P1')
 sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'alunosprite.png')).convert_alpha()
 nuvem_sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'clouds-small.png')).convert_alpha()
 ground_sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'Basic_Ground.png')).convert_alpha()
+rock_sprite = pygame.image.load(os.path.join(diretorio_imagens, 'Rock Pile.png')).convert_alpha()
 # convert_alpha vai ignorar a transparência
+
+som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'gta-v-wasted-death-sound.mp3'))
+som_colisao.set_volume(1)
+colidiu = False
 
 
 # sprite do aluno
@@ -29,7 +35,7 @@ class Aluno(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.som_pulo = pygame.mixer.Sound(os.path.join(diretorio_sons, 'SFX_Jump_09.wav'))
-        self.som_queda = pygame.mixer.Sound(os.path.join(diretorio_sons, 'jumpland.wav'))
+        self.som_queda = pygame.mixer.Sound(os.path.join(diretorio_sons, 'jumpland.wav')) #som da queda, ainda está com defeito (Eric)
         self.som_pulo.set_volume(1)
         self.imagens_aluno = []
         for i in range(7):
@@ -40,6 +46,7 @@ class Aluno(pygame.sprite.Sprite):
         self.index_lista = 0    
         self.image = self.imagens_aluno[int(self.index_lista)]
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
         self.pos_y_inicial = ALTURA - 125 - (138//2)  #Variavel pra poder saber a posição inicial do personagem e poder realizar o pulo
         self.rect.center = (100,ALTURA - 125)
         self.pulo = False
@@ -50,12 +57,12 @@ class Aluno(pygame.sprite.Sprite):
     def update(self):
         #quando é teclado a barra de espaço o self.pulo == True daí vamos animar o movimento
         if self.pulo == True:
-            if self.rect.y <= 200:
+            if self.rect.y <= 150:
                 self.pulo = False
             self.rect.y -= 20
         else:
             if self.rect.y < self.pos_y_inicial:
-                self.rect.y += 20    
+                self.rect.y += 15    
             else:
                 self.rect.y = self.pos_y_inicial
                 
@@ -93,6 +100,19 @@ class Ground(pygame.sprite.Sprite):
         if self.rect.topright[0] < 0:
             self.rect.x = LARGURA
         self.rect.x -= 10
+#classe pedra
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = rock_sprite.subsurface((0,0), (160,160))
+        self.image = pygame.transform.scale(self.image, (160/3, 160/3))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.center = (LARGURA,ALTURA - 88 )
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+        self.rect.x -= 10
 
 # adicionando as sprites    
 all_sprites = pygame.sprite.Group()
@@ -106,6 +126,15 @@ all_sprites.add(nuvem)
 for i in range(LARGURA*3//64):
     ground = Ground(i)
     all_sprites.add(ground)
+
+rock = Rock()
+all_sprites.add(rock)
+
+
+# criando o grupo onde vai ser inserido os obstáculos que colidirem
+group_obstacles = pygame.sprite.Group()
+group_obstacles.add(rock)
+
 
 #Eventos do jogo
 relogio = pygame.time.Clock()
@@ -123,8 +152,24 @@ while True:
                     pass
                 else:
                     aluno.pular()
-#adicionando na tela as sprites criadas
+
+
+    colisoes = pygame.sprite.spritecollide(aluno, group_obstacles, False, pygame.sprite.collide_mask) 
+    
     all_sprites.draw(tela)
-    all_sprites.update()
+
+    if colisoes and colidiu == False:
+        som_colisao.play()
+        colidiu = True
+
+    if colidiu == True:
+        pass
+    else:
+        all_sprites.update()
+#adicionando na tela as sprites criadas
+
+
 
     pygame.display.flip()
+
+
